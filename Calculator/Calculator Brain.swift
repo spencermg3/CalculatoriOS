a1a5535c0ce63bbd0ad26ef3357ba6cd0174f1ea
spencermg3 <sgrant1@macalester.edu>
2017-02-12 20:05:35 -0600

    Initial Commit

---
+++ Calculator/Calculator Brain.swift
@@ -0,0 +1,31 @@
+//
+//  Calculator Brain.swift
+//  Calculator
+//
+//  Created by Spencer Grant on 2/11/17.
+//  Copyright © 2017 Spencer Grant. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+    private var accumulator = 0.0
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+    }
+    
+    func performOperation(symbol: String) {
+        switch symbol {
+        case "π": accumulator = M_PI
+        case "√": accumulator = sqrt(accumulator)
+        default: break
+        }
+    }
+    
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+}
